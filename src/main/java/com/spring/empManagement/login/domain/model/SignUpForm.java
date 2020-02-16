e9f59f2e0326a234ec8285709e435e0d00fdc25a
kiyotakeshi <kiyotatakeshi0609@gmail.com>
2020-02-16 19:25:15 +0900

    add validation message

--- src/main/java/com/spring/empManagement/login/domain/model/SignUpForm.java
+++ src/main/java/com/spring/empManagement/login/domain/model/SignUpForm.java
@@ -1,8 +1,10 @@
 package com.spring.empManagement.login.domain.model;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.*;
 import java.util.Date;
 
 /**
@@ -11,14 +13,27 @@ import java.util.Date;
 @Data
 public class SignUpForm {
 
+    @NotBlank // 必須入力
+    @Email // メールアドレス形式
     private String userId;
-    private String password;
-    private String  userName;
 
+    @NotBlank
+    @Length(min = 4, max = 100) // 4~100桁
+    @Pattern(regexp = "^[a-zA-Z0-9]+$") // 半角英数字のみ
+    private String password;
+
+    @NotBlank
+    private String userName;
+
+    @NotNull // 必須入力
     @DateTimeFormat(pattern = "yyyy/MM/dd")
     private Date birthday;
 
+    @Min(20)
+    @Max(120)
     private int age;
-    private boolean marriage;
+
+    @AssertFalse  // falseのみ可能
+    private boolean marriage;  // 結婚ステータス
 
 }
